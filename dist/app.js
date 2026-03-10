628eab8b765c21f614e993b1a5f31f2b57741af9
tejaskale99 <tejaskale1556@gmail.com>
2026-03-10 14:26:43 +0530

    initial backend setup

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+import express from 'express';
+const app = express();
+// app.use(express())
+module.exports = app;
+//# sourceMappingURL=app.js.map
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import { hostname } from 'node:os';
+
+// import app from './app.js'
+const app = express()
+
+const port = 5000;
+
+app.get('/collabHub' ,(req,res)=>{
+    res.send("Backend is running")
+    console.log(`Backend Is Running 🏃`)
+})
+
+app.listen(port,()=>{
+    console.log(`Server is started 🚀 on ${port}`)
+})
